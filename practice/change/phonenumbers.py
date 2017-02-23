c49d65eaaebc179e727bb025d490ee400e293e08
Dylan Brams <dylan.brams@gmail.com>
2017-02-22 16:33:03 -0800

    Another check-in for 2/22.  Included: Blackjack update for something or other. Pretty printing in Make Change and Taxes Prettier printing in wall_painting Phone numbers initial commit Probably the first working object oriented version of Make Change etc.

--- practice/change/phonenumbers.py
+++ practice/change/phonenumbers.py
@@ -8,17 +8,24 @@ OR
 555-1212
 """
 
-
 input_number = input("Please enter a phone number.")
 if len(input_number) == 10:
-    # before the 4th character
-    # between the 4th and the 7th character [included the 4th]
-    # from the 7th on [included the 7th]
-    print(input_number[:3] + '-' + input_number[3:6] + '-' + input_number[6:])
-    print('(' + input_number[:3] + ') ' + input_number[3:6] + '-' + input_number[6:])
+    phone_parts = [input_number[:3], input_number[3:6], input_number[6:]]
+dashed_phone = '-'.join(phone_parts)
+print(dashed_phone)
+paren_phone = '({}) {}-{}'.format(phone_parts[0], phone_parts[1], phone_parts[2])
+print (paren_phone)
 
-elif len(input_number) == 7:
-    print(input_number[:3] + '-' + input_number[3:])
-else:
-    print('Invalid number length.  We\'re too lazy for international numbers.')
-
+# input_number = input("Please enter a phone number.")
+# if len(input_number) == 10:
+#     # before the 4th character
+#     # between the 4th and the 7th character [included the 4th]
+#     # from the 7th on [included the 7th]
+#     #print(input_number[:3] + '-' + input_number[3:6] + '-' + input_number[6:])
+#     #print('(' + input_number[:3] + ') ' + input_number[3:6] + '-' + input_number[6:])
+#
+# elif len(input_number) == 7:
+#     print(input_number[:3] + '-' + input_number[3:])
+# else:
+#     print('Invalid number length.  We\'re too lazy for international numbers.')
+#
