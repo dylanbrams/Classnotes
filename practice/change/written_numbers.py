b3fcee2c7fac4dce67efb49400a74eac35ca0b26
Dylan Brams <dylan.brams@gmail.com>
2017-02-24 10:31:07 -0800

    Now a completely functional approach.  Also uses a dictionary for looking up words.

--- practice/change/written_numbers.py
+++ practice/change/written_numbers.py
@@ -4,7 +4,7 @@ Written Numbers:
 """
 
 words_lookup = {1: "one", 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine',
-                10: 'ten', 11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', 15: 'fifteen',
+                0: "zero", 10: 'ten', 11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', 15: 'fifteen',
                 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen',
                 20: 'twenty', 30: 'thirty', 40: 'forty', 50: 'fifty',
                 60: 'sixty', 70: 'seventy', 80: 'eighty', 90: 'ninety'}
@@ -13,29 +13,44 @@ def print_single(print_num, words_lookup):
     """
     returns a printable string representing a single-word number.  E.G. : 9
 
+    :param print_num: Number to print
+    :param words_lookup: Words to be looked up.
+    :return:
+
     >>> print_single(4, words_lookup)
     'four'
     >>> print_single(14, words_lookup)
     'fourteen'
 
-    :param print_num:
-    :param words_lookup:
-    :return:
+
     """
     print_string = words_lookup[int(print_num)]
 
     return print_string
 
 def print_two_part(print_num, words_lookup):
+    """
+    Prints a two-part number word
+    :param print_num: Integer
+    :param words_lookup: Global (for testing ease) dictionary of number words
+    :return:
+    """
     addition_str = ''
     ones_place = int(print_num) % 10
     tens_place = int(print_num // 10) * 10
     tens_word = words_lookup[tens_place]
-    ones_word = words_lookup[ones_place]
-    addition_str += tens_word + "-" + ones_word
+    addition_str += tens_word
+    if (ones_place != None and ones_place != 0):
+        ones_word = words_lookup[ones_place]
+        addition_str += "-" + ones_word
     return addition_str
 
 def print_number(print_num):
+    """
+    Prints a number (provided as integer) as a word.
+    :param print_num:
+    :return:
+    """
     print_word = str(print_num) + ' '
     if (print_num < 21):
         print_word += print_single(print_num, words_lookup)
